ac0b725b1d3b9b381face8732b15343d16c38a8c
Adeyeye <x638132@am.abg.com>
2020-01-22 18:09:24 -0600

    Latest Commit 01-22-2020

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,22 +1,63 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import ContactContext from '../../context/contact/contactContext';
 
 const ContactItem = ({ contact }) => {
+  const contactContext = useContext(ContactContext);
+
+  const { deleteContact, setCurrent, clearCurrent, current } = contactContext;
+
   const { id, name, email, phone, type } = contact;
 
   var checktype = type === 'professional';
+
+  const onDelete = () => {
+    deleteContact(id);
+    if (current !== null && current.id === contact.id) {
+      clearCurrent();
+    }
+  };
 
   return (
     <div className='card bg-light'>
       <h3 className='text-primary text-left'>
         {name}{' '}
         <span
+          style={{ float: 'right' }}
           className={checktype ? 'badge badge-primary' : 'badge badge-success'}
         >
-          {type}
+          {type.charAt(0).toUpperCase() + type.slice(1)}
         </span>
       </h3>
+      <ul className='list'>
+        {email && (
+          <li>
+            <i className='fas fa-envelope-open'></i>
+            {email}
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className='fas fa-phone'></i>
+            {phone}
+          </li>
+        )}
+      </ul>
+      <button
+        className='btn btn-dark btn-sm'
+        onClick={() => setCurrent(contact)}
+      >
+        Edit
+      </button>
+      <button className='btn btn-danger btn-sm' onClick={onDelete}>
+        Delete
+      </button>
     </div>
   );
+};
+
+ContactItem.propTypes = {
+  contact: PropTypes.object.isRequired
 };
 
 export default ContactItem;
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Contacts from '../contacts/Contacts';
+import ContactForm from '../contacts/ContactForm';
+import ContactFilter from '../contacts/ContactFilter';
 
 const Home = () => {
   return (
     <div className='grid-2'>
-      <div>{/* contactform */}</div>
       <div>
+        <ContactForm></ContactForm>
+      </div>
+      <div>
+        <ContactFilter></ContactFilter>
         <Contacts></Contacts>
       </div>
     </div>
